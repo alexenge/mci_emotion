c91a596d755f00ac9227ca089c9f596a031a2d56
Alexander Enge <alexander.enge@hu-berlin.de>
2021-02-24 19:41:00 +0100

    add new table for pilot rating data summary

--- scripts/F02_mci_emotion_pre_ratings.R
+++ scripts/F02_mci_emotion_pre_ratings.R
@@ -21,7 +21,8 @@ rmarkdown::render(input = rstudioapi::getSourceEditorContext()$path,
 # Load packages
 library(tidyverse)    # Version 1.3.0
 library(magrittr)     # Version 1.5
-library(emmeans)      # version 1.4.8
+library(emmeans)      # Version 1.4.8
+library(huxtable)     # Version 5.0.0
 
 # Load pre-rating data from SPSS file
 pilot <- haven::read_sav("FB/gesamt_2.sav")
@@ -76,6 +77,42 @@ mean(anova_metaphoricity$`VP:semantics`$residuals^2) # Mean squared error of the
 
 # Metaphoricity
 (pairwise_metaphoricity <- anova_metaphoricity %>% emmeans(specs = pairwise ~ semantics) %>% summary(adjust = "holm"))
+
+## TABLES ## ----------------------------------------------------------------------------------------------------------
+
+# Create a table summarizing the rating data in the paper
+map2(
+  c("Frage1", "Frage2", "Frage3", "Frage4"),
+  c("Cloze probability", "Plausibility", "Metaphoricity", "Imageability"),
+  function(old_name, new_name){
+    pilot %>%
+      # Give proper names to our semantic conditions
+      mutate(semantics = fct_recode(semantics, Intuitive = "int", Violation = "vio", MCI = "mci")) %>%
+      # Summarize the data (see http://www.cookbook-r.com/Graphs/Plotting_means_and_error_bars_(ggplot2)/)
+      Rmisc::summarySEwithin(measurevar = old_name, withinvars = "semantics") %>%
+      rename(m = all_of(old_name)) %>%
+      # Re-format the confidence interval
+      mutate(lower = m - ci, upper = m + ci) %>%
+      select(semantics, m, sd, lower, upper) %>%
+      mutate(across(-semantics, .fns = ~format(round(.x, 2), nsmall = 2, trim = TRUE))) %>%
+      mutate(ci = paste0("[", lower, ", ", upper, "]"), .keep = "unused") %>%
+      # Re-shape so that semantic conditions are columns and different stats are rows
+      pivot_longer(-semantics, names_to = " ") %>%
+      spread(semantics, value) %>%
+      # Give proper names to the different stats and order them
+      mutate(` ` = factor(` `, levels = c("m", "sd", "ci"), labels = c("M", "SD", "95% CI"))) %>%
+      arrange(` `) %>%
+      # Add a header row for the name of the dependent (rating) variable
+      bind_rows(c(` ` = new_name, Intuitive = "", Violation = "", MCI = ""), .)
+  }) %>%
+  # Bind all ratings together
+  bind_rows() %>%
+  # Convert to huxtable and style
+  huxtable() %>%
+  set_bold(row = c(1, seq(2, 17, by = 4)), value = TRUE) %>%
+  set_italic(row = c(seq(3, 17, by = 4), seq(4, 17, by = 4)), col = 1, value = TRUE) %T>%
+  # Export to a Word file
+  quick_docx(file = "EEG/tables/table_pilot.docx")
 
 # System specs and package versions
 sessionInfo()
